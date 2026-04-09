05f7c0e4f8050281a9d84e467be0fb826db04b85
Liem <151177983+Liem161@users.noreply.github.com>
2026-04-09 18:30:36 +0500

    Adminwho для простых смертных (#139)
    * Update AdminWhoCommand.cs
    
    * comm edit
    
    ---------
    
    Co-authored-by: Shegare <228shadowfox228@gmail.com>

--- Content.Server/Administration/Commands/AdminWhoCommand.cs
+++ Content.Server/Administration/Commands/AdminWhoCommand.cs
@@ -7,7 +7,7 @@ using Robust.Shared.Utility;
 
 namespace Content.Server.Administration.Commands;
 
-[AdminCommand(AdminFlags.AdminWho)]
+[AnyCommand] // SIS-Command_For_Everyone
 public sealed class AdminWhoCommand : LocalizedCommands
 {
     [Dependency] private readonly IAfkManager _afkManager = default!;
